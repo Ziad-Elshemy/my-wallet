c5f4f9e134c90e4ae75451f50d0e304049a02a10
Mokhtar Dewydar <123651449+mokhtardewydar2427@users.noreply.github.com>
2023-03-19 11:11:53 +0200

    Wallet Views ->My Transaction Screen

---
+++ app/src/main/java/com/easy_pro_code/wallet/HomeFlow/Presentation/myTransaction/MyTransactionFragment.kt
@@ -0,0 +1,22 @@
+package com.easy_pro_code.wallet.HomeFlow.Presentation.myTransaction
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.easy_pro_code.wallet.R
+
+
+class MyTransactionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_transaction, container, false)
+    }
+
+
+}
