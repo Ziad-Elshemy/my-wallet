a6c80816d70473653f9da8b2e76471552caa8504
michaelMagdy0 <127121631+michaelMagdy0@users.noreply.github.com>
2023-03-27 18:11:54 +0200

    profile

--- app/src/main/java/com/easy_pro_code/wallet/HomeFlow/Presentation/MyProfile/MyProfileFragment.kt
+++ app/src/main/java/com/easy_pro_code/wallet/HomeFlow/Presentation/MyProfile/MyProfileFragment.kt
@@ -5,17 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.easy_pro_code.wallet.R
+import com.easy_pro_code.wallet.data.model.remote_firebase.AuthUtils
+import com.easy_pro_code.wallet.databinding.FragmentMyProfileBinding
 
 
 class MyProfileFragment : Fragment() {
+
+    lateinit var binding : FragmentMyProfileBinding
+    val sessionManager = AuthUtils.manager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_my_profile, container, false)
+
+        binding.myAccountName.setText(AuthUtils.manager.fetchData().userName)
+
+        binding.name.setText(AuthUtils.manager.fetchData().userName)
+        binding.name.isEnabled = false
+
+        binding.phone.setText(AuthUtils.manager.getPhone())
+        binding.phone.isEnabled = false
+
+        return binding.root
     }
 
 }
