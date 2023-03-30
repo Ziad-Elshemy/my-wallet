9c3cfa2d0161157bea20b9b4174bf7da9aeac89c
poulageorge <poula.geroge@chesstrending.com>
2023-03-30 11:45:09 +0200

    RegisterationFragment

--- app/src/main/java/com/easy_pro_code/wallet/HomeFlow/Presentation/HomeActivity.kt
+++ app/src/main/java/com/easy_pro_code/wallet/HomeFlow/Presentation/HomeActivity.kt
@@ -2,11 +2,25 @@ package com.easy_pro_code.wallet.HomeFlow.Presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
 import com.easy_pro_code.wallet.R
+import com.easy_pro_code.wallet.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var dataBinding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        dataBinding=ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(dataBinding.root)
+        val navController = Navigation.findNavController(this,R.id.fragmentContainerView)
+        dataBinding.btnHomePage.setOnClickListener {
+            navController.navigate(R.id.homeFragment,null,NavOptions.Builder().setPopUpTo(navController.graph.startDestinationId, true).build())
+        }
+        dataBinding.btnMenu.setOnClickListener {
+            navController.navigate(R.id.menuFragment,null,NavOptions.Builder().setPopUpTo(R.id.menuFragment, true).build())
+        }
     }
 }
